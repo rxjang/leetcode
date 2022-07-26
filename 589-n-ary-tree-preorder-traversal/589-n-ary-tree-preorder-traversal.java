b70941c78cabc376289efb3c2dfa3f2ec9178389
장서현 <68838251+rxjang@users.noreply.github.com>
2022-07-26 16:41:02 +0900

    Time: 1 ms (90.36%), Space: 46.5 MB (24.06%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> answer = new ArrayList<>();
+    
+    public List<Integer> preorder(Node root) {
+        if (root == null) return answer;
+        
+        answer.add(root.val);
+        
+        for(Node node: root.children) {
+            preorder(node);   
+        }
+        
+        return answer;
+    }
+}
