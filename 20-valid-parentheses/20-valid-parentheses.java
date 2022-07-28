8b941964fc78151569626d4ac37710c387a3360f
장서현 <68838251+rxjang@users.noreply.github.com>
2022-07-28 09:34:02 +0900

    Time: 2 ms (90.26%), Space: 42.4 MB (23.59%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else {
+                char temp = stack.pop();
+                if (temp == '(' && c == ')') {
+                    continue;
+                } else if (temp == '[' && c == ']') {
+                    continue;
+                } else if (temp == '{' && c == '}') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
