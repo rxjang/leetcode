34d54b7564897d9217e0cc56477528aef0f59c1a
장서현 <68838251+rxjang@users.noreply.github.com>
2022-07-28 09:17:56 +0900

    Time: 3 ms (51.30%), Space: 43.1 MB (6.36%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -2,25 +2,22 @@ class Solution {
     public boolean backspaceCompare(String s, String t) {
         s = process(s);
         t = process(t);
-        System.out.println(s);
-        System.out.println(t);
         
         return s.equals(t);
     }
     
     
     private String process(String text) {
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         
         for (char c: text.toCharArray()) {
-            if (c == '#') {
-                if (!stack.isEmpty()) {
-                    stack.pop();                    
-                }
-            } else {
-                stack.push(String.valueOf(c));
-            }
+           if (c != '#') {
+                stack.push(c);               
+           } else if (!stack.empty()) {
+                stack.pop();               
+           }
         }
-        return String.join("", stack);
+        
+        return String.valueOf(stack);
     }
 }
