12b31a90a7c077056ef1b74fe21d5810a78762fa
장서현 <68838251+rxjang@users.noreply.github.com>
2022-08-13 12:52:21 +0900

    Time: 5 ms (87.20%), Space: 48.7 MB (36.65%) - LeetHub

---
+++ 641-design-circular-deque/641-design-circular-deque.java
@@ -0,0 +1,76 @@
+class MyCircularDeque {
+    int[] arr;
+    int head, tail, size, n;
+
+    public MyCircularDeque(int k) {
+        arr = new int[k];
+        n = arr.length;
+        head = 0;
+        size = 0;
+        tail = 1;
+    }
+    
+    public boolean insertFront(int value) {
+        if (size == n) return false;
+        
+        head = ++head % n;
+        arr[head] = value;
+        size ++;
+        return true;
+    }
+    
+    public boolean insertLast(int value) {
+        if (size == n) return false;
+        
+        tail = (--tail + n) % n;
+        arr[tail] = value;
+        
+        size ++;
+        return true;
+    }
+    
+    public boolean deleteFront() {
+        if (size == 0) return false;
+        
+        head = (--head + n) % n;
+        size --;
+        return true;
+    }
+    
+    public boolean deleteLast() {
+        if (size == 0) return false;
+        
+        tail = ++tail % n;
+        size --;
+        return true;
+    }
+    
+    public int getFront() {
+        return size == 0 ? -1 : arr[head];
+    }
+    
+    public int getRear() {
+        return size == 0 ? -1 : arr[tail];
+    }
+    
+    public boolean isEmpty() {
+        return size == 0;   
+    }
+    
+    public boolean isFull() {
+        return size == n;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
