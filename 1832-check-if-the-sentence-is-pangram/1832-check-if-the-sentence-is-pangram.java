9dca6dbaa2dda7207fa75da73e6778dce53b033b
장서현 <68838251+rxjang@users.noreply.github.com>
2022-09-19 22:35:36 +0900

    Time: 2 ms (68.06%), Space: 40.4 MB (93.48%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -2,6 +2,10 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         Set<Character> set = new HashSet<>();
         
+        if (sentence.length() < 26) {
+            return false;
+        }
+        
         for (char c: sentence.toCharArray()) {
             set.add(c);
         }
